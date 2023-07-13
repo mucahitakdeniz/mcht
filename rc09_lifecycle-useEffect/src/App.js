6382c38de134f0d3f362b8bb6552ddaec4ea8527
mucahit <mucahitakdeniz28@gmail.com>
2023-07-13 21:30:36 +0300

    lifecycl

--- rc09_lifecycle-useEffect/src/App.js
+++ rc09_lifecycle-useEffect/src/App.js
@@ -1,9 +1,15 @@
 import LifeCycleMethods from "./components/LifeCycleMethods"
+import { useState } from "react"
 
 function App() {
+  const [visible, setVisible] = useState(false)
   return (
     <div className="container text-center mt-4">
-      <LifeCycleMethods />
+      <button className="btn btn-danger" onClick={() => setVisible(!visible)}>
+        Show/Hide
+      </button>
+
+      {visible && <LifeCycleMethods />}
     </div>
   )
 }
