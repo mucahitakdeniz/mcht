1bba19fe722ad1ac92803f32b2aa292890ebdf56
Mucahit Akdeniz <mucahitakdeniz28@gmail.com>
2023-07-14 19:00:23 +0300

    hata

--- 003-React-ToDo-App/src/components/TodoList.jsx
+++ 003-React-ToDo-App/src/components/TodoList.jsx
@@ -1,42 +1,44 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
-import { useState } from "react";
+import { Children, useState } from "react";
+import data from "../helper/data";
 const TodoList = () => {
-  let count = 0;
-  let array = [];
+  let x = [];
+  const myTimeout = setTimeout(arraylist, 100);
+  const arraylist = () => {
+    x = [...TodoList];
+  };
   const [input, setInput] = useState("");
   const [todoArray, setTodoArray] = useState([]);
-  const handleClick = (e) => {
-    setTodoArray(array.push(input))
-    setInput("")
-
-  
-    
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    console.log(input);
   };
-  return (
-    <>
-      <InputGroup className="text-center m-auto mb-3" style={{ width: "80%" }}>
-        <Form.Control
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="Enter new todo"
-          value={input}
-        />
-        <Button
-          type="submit"
-          onClick={handleClick}
-          variant="text-light bg-success"
-          id="button"
-        >
-          Add Todo
-        </Button>
-      </InputGroup>
-      <h2 className="text-center fs-1">Todos</h2>
-      {/* {x.map((e) => {
-        return <h1>{e}</h1>;
-      })} */}
-    </>
-  );
-};
+  const handleClick = (e) => {
+    setTodoArray([...todoArray, input]);
+    setInput("");
+  };
+}
+return (
+  <>
+    <InputGroup className="text-center m-auto mb-3" style={{ width: "80%" }}>
+      <Form.Control
+        onChange={handleChange}
+        placeholder="Enter new todo"
+        value={input}
+      />
+      <Button
+        type="submit"
+        onClick={handleClick}
+        variant="text-light bg-success"
+        id="button"
+      >
+        Add Todo
+      </Button>
+    </InputGroup>
+    <h2 className="text-center fs-1">Todos {input}</h2>
+  </>
+)
 
 export default TodoList;
