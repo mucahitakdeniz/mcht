baf3190d21ace4d8c5f0a6bc4aedecec39c503c4
mucahit <mucahitakdeniz28@gmail.com>
2023-07-17 21:15:44 +0300

    start

--- rc10_tutorial-app/tutorial-app-inClass/src/pages/Home.jsx
+++ rc10_tutorial-app/tutorial-app-inClass/src/pages/Home.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import axios from "axios";
 
 const Home = () => {
+  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/";
+  const [tutorials, setTutorials] = useState("");
+  const getTutorials = async () => {
+    const data = await axios(BASE_URL);
+    setTutorials(data.data)
+    console.log(data);
+  };
+  useEffect(()=>{
+    getTutorials()
+  },[])
   return (
     <>
       <AddTutorial />
