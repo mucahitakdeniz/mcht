ab9c8827ea027f75a8c8f0bdd1fad66596bef93a
Mucahit Akdeniz <mucahitakdeniz28@gmail.com>
2023-07-19 14:51:50 +0300

    start

--- rc10_tutorial-app/tutorial-app-starter/src/pages/Home.jsx
+++ rc10_tutorial-app/tutorial-app-starter/src/pages/Home.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import axios from "axios";
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState([]);
+  const Base_Url = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+  const getTutorials = async () => {
+    try {
+      const { data } = await axios(Base_Url);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTutorials();
+  }, []);
+
   return (
     <>
       <AddTutorial />
-      <TutorialList />
+      <TutorialList tutorials={tutorials} />
     </>
   );
 };
