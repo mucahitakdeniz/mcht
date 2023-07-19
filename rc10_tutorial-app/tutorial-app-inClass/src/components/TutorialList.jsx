846ccc70d8a44c4a0be1cdb551262675b25ec077
mucahit <mucahitakdeniz28@gmail.com>
2023-07-19 19:31:56 +0300

    a

--- rc10_tutorial-app/tutorial-app-inClass/src/components/TutorialList.jsx
+++ rc10_tutorial-app/tutorial-app-inClass/src/components/TutorialList.jsx
@@ -1,65 +1,70 @@
-import { FaEdit } from "react-icons/fa"
-import { AiFillDelete } from "react-icons/ai"
+import { useState } from "react";
+import axios from "axios";
 
-const TutorialList = ({ tutorials }) => {
-  // const tutorials = [
-  //   {
-  //     id: 1,
-  //     title: "JS",
-  //     description: "JS is a programming language",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "React",
-  //     description: "JS library for UI design",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "VUE",
-  //     description: "JS library for UI design",
-  //   },
-  // ]
+const AddTutorial = ({ getTutorials }) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTutor = { title: title, description: description };
+    console.log(newTutor);
+    postTutorial(newTutor);
+    setTitle("");
+    setDescription("");
+  };
+
+  const postTutorial = async (newTutor) => {
+    const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+    try {
+      const res = await axios.post(BASE_URL, newTutor);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+
+    //? Tum tutorial'lari iste ve state'i guncelle
+    getTutorials();
+  };
 
   return (
-    <div className="container mt-4">
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">#id</th>
-            <th scope="col">Title</th>
-            <th scope="col">Description</th>
-            <th scope="col" className="text-center">
-              Edit
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {tutorials?.map((item) => {
-            const { id, title, description } = item
-            return (
-              <tr key={id}>
-                <th>{id}</th>
-                <td>{title}</td>
-                <td>{description}</td>
-                <td className="text-center text-nowrap">
-                  <FaEdit
-                    size={20}
-                    type="button"
-                    className="me-2 text-warning"
-                  />
-                  <AiFillDelete
-                    size={22}
-                    type="button"
-                    className="text-danger "
-                  />
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+    <div className="container text-center mt-4">
+      <h1 className="display-6 text-danger">Add Your Tutorial</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            placeholder="Enter your title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="desc" className="form-label">
+            Description
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="desc"
+            placeholder="Enter your Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-danger mb-4">
+          Submit
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default TutorialList
+export default AddTutorial;
