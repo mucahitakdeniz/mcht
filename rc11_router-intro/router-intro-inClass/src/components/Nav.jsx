3e9cd15754be79840052e251b3a9e36920879009
mucahit <mucahitakdeniz28@gmail.com>
2023-07-19 21:04:19 +0300

    active

--- rc11_router-intro/router-intro-inClass/src/components/Nav.jsx
+++ rc11_router-intro/router-intro-inClass/src/components/Nav.jsx
@@ -1,14 +1,28 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Nav = () => {
+  //? Link, NavLink ve Navigate componentleri declarative routing
+  //? gerceklestirmek icin kullanilir.
+  //? Ornegin: Link ve NavLink Sayfada gorulebilen, kullanciyla
+  //? bir etkilesim icerisinde bulunarak yonledirme yapilan bir
+  //? componentlerdir. (Nav v.b)
   return (
     <ul className="bg-warning">
       <li>
-        <a href="/">Home</a>
+        <Link to="/">Home</Link>
       </li>
       <li>
-        <a href="/instructors">Instructors</a>
+        {/* <NavLink
+          style={({ isActive }) => ({ color: isActive && "red" })}
+          to="/instructors"
+        >
+          Instructors
+        </NavLink> */}
+        {/* //? Navlink kullanıldığında aktif olan link elementine otomatik active class'ı eklenir, bu class stillendirme için kullanılabilir */}
+        <NavLink to="/instructors">Instructors</NavLink>
       </li>
       <li>
-        <a href="/contact">Contact</a>
+        <NavLink to="/contact">Contact</NavLink>
       </li>
     </ul>
   );
