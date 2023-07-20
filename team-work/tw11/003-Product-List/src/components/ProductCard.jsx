a8fe81157f78e17b694d90f15e9453f295c4be2f
Mucahit Akdeniz <mucahitakdeniz28@gmail.com>
2023-07-20 18:59:34 +0300

    rowcol

--- team-work/tw11/003-Product-List/src/components/ProductCard.jsx
+++ team-work/tw11/003-Product-List/src/components/ProductCard.jsx
@@ -22,24 +22,28 @@ const ProductCard = () => {
 
   return (
     <>
-      {prod.map((item) => {
-        <Card key={item.id} style={{ width: "18rem" }}>
-          <Card.Img src="{item.image}" />
-          <Card.Body>
-            <Card.Title>{item.title}</Card.Title>
-            <Card.Text>{item.description}</Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-          </ListGroup>
-          <Card.Body>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-          </Card.Body>
-        </Card>;
-      })}
+      {prod.map((item) => (
+        <div className="row container">
+          <div className="col-md-12 col-l-4">
+            <Card key={item.id} style={{ width: "18rem" }}>
+              <Card.Img src={item.image} />
+              <Card.Body>
+                <Card.Title>{item.title}</Card.Title>
+                <Card.Text>{item.description}</Card.Text>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+              </ListGroup>
+              <Card.Body>
+                <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      ))}
     </>
   );
 };
