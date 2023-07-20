be6c13c99032e7296e132b3ed625e5f4342441a8
Mucahit Akdeniz <mucahitakdeniz28@gmail.com>
2023-07-20 16:30:01 +0300

    header

--- team-work/tw11/003-Product-List/src/components/Header.jsx
+++ team-work/tw11/003-Product-List/src/components/Header.jsx
@@ -3,37 +3,39 @@ const Header = () => {
   return (
     <>
       <h1 className="text-center mt-5 ">Products List</h1>
-      <div className="text-center  b ">
-        <Button
-          className="rounded me-2 text-black bg-light-subtle me-2 "
-          href="#"
-        >
-          ALL
-        </Button>
-        <Button
-          className="rounded me-2 text-black bg-light-subtle me-2 "
-          href="#"
-        >
-          ELECTRONİCS
-        </Button>
-        <Button
-          className="rounded me-2 text-black bg-light-subtle me-2 "
-          href="#"
-        >
-          jEWELERY
-        </Button>
-        <Button
-          className="rounded me-2 text-black bg-light-subtle me-2 "
-          href="#"
-        >
-          MEN'S CLOTHİNG
-        </Button>
-        <Button
-          className="rounded me-2 text-black bg-light-subtle me-2 "
-          href="#"
-        >
-          WOMEN'S CLOTHİNG
-        </Button>
+      <div className="text-center container">
+        <div className="row">
+          <Button
+            className="btn rounded me-2 text-black bg-light-subtle me-2 col-12 col-md-1 "
+            href="#"
+          >
+            ALL
+          </Button>
+          <Button
+            className="btn rounded me-2 text-black bg-light-subtle me-2 col-12 col-md-2 "
+            href="#"
+          >
+            ELECTRONİCS
+          </Button>
+          <Button
+            className="btn rounded me-2 text-black bg-light-subtle me-2 col-12 col-md-2"
+            href="#"
+          >
+            jEWELERY
+          </Button>
+          <Button
+            className="btn rounded me-2 text-black bg-light-subtle me-2 col-12 col-md-2"
+            href="#"
+          >
+            MEN'S CLOTHİNG
+          </Button>
+          <Button
+            className="btn rounded me-2 text-black bg-light-subtle me-2 col-12 col-md-2"
+            href="#"
+          >
+            WOMEN'S CLOTHİNG
+          </Button>
+        </div>
       </div>
     </>
   );
