834ea927afd100001ff09eb073c618e88612a001
Mucahit Akdeniz <mucahitakdeniz28@gmail.com>
2023-07-21 16:20:40 +0300

    categorys

--- team-work/tw11/003-Product-List/src/components/ProductCard.jsx
+++ team-work/tw11/003-Product-List/src/components/ProductCard.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 
-const ProductCard = () => {
+const ProductCard = ({ filt }) => {
   const [prod, setProd] = useState([]);
 
   useEffect(() => {
@@ -23,26 +23,31 @@ const ProductCard = () => {
   return (
     <>
       <div className="row m-auto">
-        {prod.map((item) => (
-          <div className="col-xs-12 col-sm-6  col-md-4 col-lg-3 text-center">
-            <Card key={item.id} className="cards" style={{ width: "18rem" }}>
-              <Card.Img src={item.image} />
-              <Card.Body>
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Text>{item.description}</Card.Text>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-              </ListGroup>
-              <Card.Body>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
-              </Card.Body>
-            </Card>
-          </div>
-        ))}
+        {prod
+          .filter((e) => (filt ? e.category === filt : e))
+          .map((item) => (
+            <div
+              key={item.id}
+              className="col-xs-12 col-sm-6  col-md-4 col-lg-3 text-center"
+            >
+              <Card key={item.id} className="cards" style={{ width: "18rem" }}>
+                <Card.Img src={item.image} />
+                <Card.Body>
+                  <Card.Title>{item.title}</Card.Title>
+                  <Card.Text>{item.description}</Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                  <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                  <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                  <Card.Link href="#">Card Link</Card.Link>
+                  <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
       </div>
     </>
   );
--- team-work/tw11/003-Product-List/src/pages/Home.jsx
+++ team-work/tw11/003-Product-List/src/pages/Home.jsx
@@ -3,13 +3,13 @@ import { useState } from "react";
 import ProductCard from "../components/ProductCard";
 
 const Home = () => {
-  const [filt, setFilt] = useState();
+  const [filt, setFilt] = useState("");
   const Product_Category = "https://fakestoreapi.com/products/categories/";
   const Product_List = "https://fakestoreapi.com/products";
   return (
     <>
-      <Header />
-      <ProductCard />
+      <Header setFilt={setFilt} />
+      <ProductCard filt={filt} />
     </>
   );
 };
