97a24d042222fba3f616531bf74a75a96c1f1ecb
Mucahit Akdeniz <mucahitakdeniz28@gmail.com>
2023-07-22 17:05:32 +0300

    home

--- team-work/tw11/Product-List-inClass/src/pages/Home.jsx
+++ team-work/tw11/Product-List-inClass/src/pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Header } from "react-bootstrap/lib/Modal";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
+  const getProduct = () => {
+    const BASE_URL = " https://fakestoreapi.com/products";
+  };
+  useEffect (() =>{
+    get
+  },[])
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Header />
+      <ProductCard />
+    </>
+  );
+};
 
-export default Home
+export default Home;
