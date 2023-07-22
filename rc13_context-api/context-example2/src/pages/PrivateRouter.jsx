0fb51adb01eb754a428e02408db4a581351e4ee0
Mucahit Akdeniz <mucahitakdeniz28@gmail.com>
2023-07-22 13:49:09 +0300

    example2

--- rc13_context-api/context-example2/src/pages/PrivateRouter.jsx
+++ rc13_context-api/context-example2/src/pages/PrivateRouter.jsx
@@ -1,8 +1,10 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { useContext } from "react"
+import { Outlet, Navigate } from "react-router-dom"
+import { useLoginContext } from "../context/LoginProvider"
 
 const PrivateRouter = () => {
-  const user = true;
-  return user ? <Outlet /> : <Navigate to="/login" />;
-};
+  const { user } = useLoginContext()
+  return user?.email && user?.password ? <Outlet /> : <Navigate to="/login" />
+}
 
-export default PrivateRouter;
+export default PrivateRouter
