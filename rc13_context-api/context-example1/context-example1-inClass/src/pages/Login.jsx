93bca269a03267bee94f906584bfc4733494d454
Mucahit Akdeniz <mucahitakdeniz28@gmail.com>
2023-07-22 13:19:33 +0300

    login_logout

--- rc13_context-api/context-example1/context-example1-inClass/src/pages/Login.jsx
+++ rc13_context-api/context-example1/context-example1-inClass/src/pages/Login.jsx
@@ -1,15 +1,23 @@
-import { useState } from "react"
+import { useContext } from "react"
 import Container from "react-bootstrap/Container"
 import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form"
+import { LoginContext } from "../context/LoginContext"
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
-  //! Local State
+  // //! Local State
+  // const [user, setUser] = useState({ email: "", password: "" })
 
-  const [user, setUser] = useState({ email: "", password: "" })
+  //? Consuming of login context
+  const { user, setUser } = useContext(LoginContext)
+
+  const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    navigate("/")
+    // setUser({ email: "", password: "" })
   }
 
   return (
@@ -23,6 +31,7 @@ const Login = () => {
             placeholder="Enter your email"
             name="email"
             value={user?.email}
+            required
             onChange={(e) => setUser({ ...user, email: e.target.value })}
           />
         </Form.Group>
@@ -34,6 +43,7 @@ const Login = () => {
             placeholder="Enter your password"
             name="password"
             value={user?.password}
+            required
             onChange={(e) => setUser({ ...user, password: e.target.value })}
           />
         </Form.Group>
