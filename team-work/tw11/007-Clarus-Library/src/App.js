defa1549e7609ea84adad76916e672c547c0f1a9
Mucahit Akdeniz <mucahitakdeniz28@gmail.com>
2023-07-24 11:17:05 +0300

    filt

---
+++ team-work/tw11/007-Clarus-Library/src/App.js
@@ -0,0 +1,13 @@
+import Card from "./components/card/Card";
+import "./index.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
--- team-work/tw11/007-Clarus-Library/src/components/card/Card.jsx
+++ team-work/tw11/007-Clarus-Library/src/components/card/Card.jsx
@@ -1,19 +1,43 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 const Card = () => {
+  const [cards, setCards] = useState([]);
 
-
-  const getBooks = async () =>{
-    const BASE_URL="https://www.googleapis.com/books/v1/volumes?q=react&printType=books&key=AIzaSyCvTpnIzXtadNKxNDhwpspdEa8l1rcidt4"
+  const getBooks = async () => {
+    const BASE_URL =
+      "https://www.googleapis.com/books/v1/volumes?q=still&printType=magazines&key=AIzaSyCvTpnIzXtadNKxNDhwpspdEa8l1rcidt4";
     try {
-      const {data} = await axios.get(BASE_URL)
-
+      const { data } = await axios.get(BASE_URL);
+      setCards(data.items);
+    } catch (error) {
+      console.log(error);
     }
-  } 
+    console.log(cards);
+  };
+
+  useEffect(() => {
+    getBooks();
+    console.log(cards);
+  }, []);
+  console.log(cards);
+
   return (
-    <div>Card</div>
+    <div className="card-container">
+      {cards.map((book) => (
+        <div className="card" key={book.id}>
+          <img
+            src={
+              book.volumeInfo.imageLinks?.thumbnail ||
+              "https://via.placeholder.com/128x196"
+            }
+            alt={book.volumeInfo.title}
+          />
+          <h2>{book.volumeInfo.title}</h2>
+          <p>Country: {book.volumeInfo.country || "N/A"}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-    
-  )
-}
-
-export default Card
+export default Card;
