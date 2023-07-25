2aeda3e2189421e2a9d21f5a1d8e0285213d7ad0
mucahit <mucahitakdeniz28@gmail.com>
2023-07-25 23:19:39 +0300

    home

--- team-work/tw11/007-Clarus-Library/src/components/card/Card.jsx
+++ team-work/tw11/007-Clarus-Library/src/components/card/Card.jsx
@@ -9,6 +9,7 @@ const Card = ({ input, type }) => {
         `https://www.googleapis.com/books/v1/volumes?q=${input}&printType=${type}&key=AIzaSyCvTpnIzXtadNKxNDhwpspdEa8l1rcidt4`
       );
       setCards(data.items);
+      console.log(cards);
     } catch (error) {
       console.log(error);
     }
@@ -19,12 +20,26 @@ const Card = ({ input, type }) => {
     getBooks();
     console.log(cards);
   }, [input, type]);
+  useEffect(() => {
+    getBooks();
+    console.log(cards);
+  }, [type]);
+  const img =
   console.log(cards);
 
   return (
     <div className="card-container books">
       {cards.map((book) => (
         <div className="card" key={book.id}>
+          <div>
+            <h2 className="text-danger">{book.volumeInfo.title}</h2>
+<img src=".../assets/" alt="" />
+            <h3>
+              Author:
+              <span className="text-danger">{book?.volumeInfo?.authors}</span>
+            </h3>
+          </div>
+
           <img
             src={
               book.volumeInfo.imageLinks?.thumbnail ||
@@ -32,8 +47,8 @@ const Card = ({ input, type }) => {
             }
             alt={book.volumeInfo.title}
           />
-          <h2>{book.volumeInfo.title}</h2>
-          <p>Country: {book.volumeInfo.country || "N/A"}</p>
+                    <div> {img || <p>{book?.searchInfo?.textSnippet}}</p>} </div>
+
         </div>
       ))}
     </div>
--- team-work/tw11/007-Clarus-Library/src/components/header/Header.jsx
+++ team-work/tw11/007-Clarus-Library/src/components/header/Header.jsx
@@ -1,26 +1,14 @@
 import { useState } from "react";
 
 const Header = ({ setInput, setType, type }) => {
-  // const [isOpen, setIsOpen] = useState(false);
-  // const [selectedOption, setSelectedOption] = useState("all");
-
-  // const handleOptionChange = (option) => {
-  //   setSelectedOption(option);
-  // };
-
   const option = ["all", "books", "magazines"];
   const [change, setChange] = useState("");
   const [t, setT] = useState(type);
 
-  // const handleDropDown = (option) => {
-  //   setT(option);
-  //   setType(option); // Türü ana bileşende değiştirmek için setType kullanın
-  // };
-
   const handleSearch = (e) => {
     e.preventDefault();
     setInput(change);
-    setType(t); // Türü değiştirmek için t state'ini kullanın
+    setType(t); 
     setChange("");
   };
 
@@ -36,53 +24,13 @@ const Header = ({ setInput, setType, type }) => {
           value={change}
           onChange={(e) => setChange(e.target.value)}
         />
-        <div class="dropdown">
-          <button
-            class="btn btn-secondary dropdown-toggle"
-            type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Dropdown button
-          </button>
-          <ul class="dropdown-menu">
-            <li>
-              <a class="dropdown-item" href="#">
-                Action
-              </a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">
-                Another action
-              </a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">
-                Something else here
-              </a>
-            </li>
-          </ul>
-        </div>
+       
 
-        {/* <div className="dropdown">
-          <button
-            className="dropdown-toggle"
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            {selectedOption}
-          </button>
-          {isOpen && (
-            <ul className="dropdown-menu">
-              {option.map((option, index) => (
-                <li key={index}>
-                  <button >
-                    {option}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div> */}
+        <select name="cars" id="cars">
+          {option.map((op,i) => (
+            <option key={{i}} value="all" onClick={()=>setT(op)}>{op}</option>
+          ))}
+        </select>
 
         <button
           onClick={handleSearch}
