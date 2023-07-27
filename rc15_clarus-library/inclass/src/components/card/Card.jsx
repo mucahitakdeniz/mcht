a4fe1e04793665096d074bca3ec54addcfd196d2
mucahit <mucahitakdeniz28@gmail.com>
2023-07-27 22:22:49 +0300

    card

--- rc15_clarus-library/inclass/src/components/card/Card.jsx
+++ rc15_clarus-library/inclass/src/components/card/Card.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Card = () => {
-  const api= "https://www.googleapis.com/books/v1/volumes?q=${input}&printType=${type}&key=AIzaSyCvTpnIzXtadNKxNDhwpspdEa8l1rcidt4"
   return (
     <div>Card</div>
   )
