a2632f426ad8d438889725ffce8eb4252b3860a7
mucahit <mucahitakdeniz28@gmail.com>
2023-07-27 20:23:16 +0300

    home

--- rc15_clarus-library/inclass/src/pages/home/Home.jsx
+++ rc15_clarus-library/inclass/src/pages/home/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/header/Header";
+import Card from "../../components/card/Card";
+import { HomeContainer, HomeImg, CardContainer } from "./Home.style";
+import homeImg from "../../assets/books.jpg";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <HomeContainer>
+      <Header />
+      <HomeImg>
+        <img src={homeImg} alt="" />
+      </HomeImg>
+      <CardContainer>
+        <Card />
+      </CardContainer>
+    </HomeContainer>
+  );
+};
 
-export default Home
+export default Home;
