520e3384bfbe38f22354751d63c8b98dbac2d5c0
mucahit <mucahitakdeniz28@gmail.com>
2023-08-03 21:03:07 +0300

    typo2

--- rc19_mui-intro/mui-intro-vite/src/App.jsx
+++ rc19_mui-intro/mui-intro-vite/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import Typo from "./components/Typo"
 
 function App() {
   return (
     <>
-      <h1>MUI</h1>
+      <Typo />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
