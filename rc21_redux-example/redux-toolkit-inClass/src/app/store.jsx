10e6817baa7b9bf0fb78f6d3d7728a1b0e4b4e83
mucahit <mucahitakdeniz28@gmail.com>
2023-08-07 23:19:01 +0300

    news

--- rc21_redux-example/redux-toolkit-inClass/src/app/store.jsx
+++ rc21_redux-example/redux-toolkit-inClass/src/app/store.jsx
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "../features/authSlice"
+import newsReducer from "../features/authSlice"
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    // api: newsReducer,
+    api: newsReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
   //? eger gelistirme asamasi prodcution ise o zaman yukaridaki ifade false dondurur ve dolayisiyla devTool kullanima kapali olur.
--- rc21_redux-example/redux-toolkit-inClass/src/pages/News.jsx
+++ rc21_redux-example/redux-toolkit-inClass/src/pages/News.jsx
@@ -5,8 +5,17 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { CardMedia } from "@mui/material";
+import { getNews } from "../features/newsSlice";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 const News = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getNews());
+  }, []);
+
   return (
     <>
       <h1>NEWS</h1>
