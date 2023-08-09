e86f01b8c8275f1951d5bc717c4bdf38c769206c
Mücahit Akdeniz <mucahitakdeniz28@gmail.comgit config --global user.email mucahitakdeniz28@gmail.com>
2023-08-09 12:27:29 +0300

    authslice

---
+++ rc21_redux-example/redux-toolkit-starter/src/features/authSlice.jsx
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: "",
+};
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    clearUser: (state) => {
+      state.user = "";
+    },
+  },
+});
+export const { setUser, clearUser } = authSlice.actions;
+export default authSlice.reducer;
