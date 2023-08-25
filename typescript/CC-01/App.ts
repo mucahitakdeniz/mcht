d517e635c6d405c49b4d210b1b92419b7bb54b64
mucahit <mucahitakdeniz28@gmail.com>
2023-08-25 21:46:31 +0300

    btn

---
+++ typescript/CC-01/App.ts
@@ -0,0 +1,30 @@
+const tc = document.getElementById("tcInput")! as HTMLInputElement;
+const btn = document.getElementById("btn")! as HTMLButtonElement;
+const p = document.getElementById("sonuc")! as HTMLParagraphElement;
+
+const check = (x: string) => {
+  let a: number =
+    ((+x[0] + +x[2] + +x[4] + +x[6] + +x[8]) * 7 -
+      (+x[1] + +x[3] + +x[5] + +x[7])) %
+    10;
+  if (a !== +x[9]) {
+    alert("TC numarası geçersizdir");
+  } else {
+    let b: number = 0;
+
+    for (let i = 0; i < 10; i++) {
+      b += +x[i];
+    }
+    b = b % 10;
+    if (b !== +x[10]) {
+      alert("Tc kimlik numarası geçersizdir");
+    } else {
+      alert("TC kimlik numarası gecerlidir");
+    }
+  }
+};
+btn.addEventListener("click", () => {
+  const inputValue = tc.value; //
+  check(inputValue);
+  p.innerText = "Sonuç: Kontrol ediliyor...";
+});
